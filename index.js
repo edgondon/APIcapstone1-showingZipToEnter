1565fb93f3ff2765ee9f7c873249b56d1d26994d
Daniel P Landon <dougedd@yahoo.com>
2019-10-01 10:30:52 -0700

    10:30 10.01.2019

--- index.js
+++ index.js
@@ -86,12 +86,37 @@ function geoClouder(t) {
   
   }
 
+  function datesinConsole() {
+    let now = new Date();
+    let month = (now.getMonth() + 1);               
+    let day = now.getDate();
+    if (month < 10) 
+        month = "0" + month;
+    if (day < 10) 
+        day = "0" + day;
+    let today = now.getFullYear() + '-' + month + '-' + day;
+    console.log(today);
 
+    let month2 = (now.getMonth() + 2);
+    let today2 = now.getFullYear() + '-' + month2 + '-' + day;
+    console.log(today2);
+
+
+$("form").append(`
+<label for="State">Enter Radius of Search in Miles</label>
+<input type="number" id="radiuss" name="Radius" min="1" max="100" value="25" required>
+<label for="State">FIRST DATE IN RANGE YYYY-MM-DD</label>
+<input type="date" id="alpha" name="State" value="${today}" required>
+<label for="numSearch">SECOND DATE IN RANGE YYYY-MM-DD</label>
+<input type="date" id="omega" name="numSearch" value="${today2}">
+<input type="submit"  value="Submit Request">`);
+  
+};
 
 
 
 function watchEnter() {
-    
+    datesinConsole();
     $('form').submit(event => {
         event.preventDefault();
         $('#errod').empty();
