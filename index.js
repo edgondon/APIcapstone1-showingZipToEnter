df9e0a5484850c003552ccee3fe637b461dae26d
Daniel P Landon <dougedd@yahoo.com>
2019-10-03 11:22:25 -0700

    11:22 10.03.2019

--- index.js
+++ index.js
@@ -32,14 +32,20 @@ function initMap(json) {
 
 
 let longlat = [];
+let addressGo = [];
 
 function showEvents(json) {
-    for(var i=0; i<json.page.size; i++) {
+    let nums = Math.min(json.page.size, json.page.totalElements);
+    for(var i=0; i<nums; i++) {
       $("#events").append(`<p>${json._embedded.events[i].name}</p>
             <p>Date of Event: ${json._embedded.events[i].dates.start.localDate}</p>
             <p>Distance in Miles: ${json._embedded.events[i].distance}</p>
-            <p>Address: ${json._embedded.events[i]._embedded.venues[0].address.line1}, ${json._embedded.events[i]._embedded.venues[0].city.name}, ${json._embedded.events[i]._embedded.venues[0].state.stateCode}, ${json._embedded.events[i]._embedded.venues[0].postalCode} </p>
-            <a href="${json._embedded.events[i].url}" target="_blank">Link for Tickets and More</a>
+            <p>Address: ${json._embedded.events[i]._embedded.venues[0].address.line1}, ${json._embedded.events[i]._embedded.venues[0].city.name}, ${json._embedded.events[i]._embedded.venues[0].state.stateCode}, ${json._embedded.events[i]._embedded.venues[0].postalCode}</p>
+            <form id="form2">
+            <input type="text" id="start" name="startaddress" value="${json._embedded.events[i]._embedded.venues[0].address.line1}, ${json._embedded.events[i]._embedded.venues[0].city.name}, ${json._embedded.events[i]._embedded.venues[0].state.stateCode}, ${json._embedded.events[i]._embedded.venues[0].postalCode}" required>
+            <input type="radio" id="directions"  value="Get Directions">Get Directions</input>
+            </form>
+            <a href="${json._embedded.events[i].url}" target="_blank">Link for Tickets</a>
             `);
       
     }
@@ -84,7 +90,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     console.log(today2);
 
 
-$("form").append(`
+$('#form1').append(`
 <label for="Radius">Enter Radius of Search in Miles</label>
 <input type="number" id="radiuss" name="Radius" min="1" max="100" value="25" required>
 <label for="State">FIRST DATE IN RANGE YYYY-MM-DD</label>
@@ -111,7 +117,7 @@ function infosubmit (alpha, omega, radiuss) {
                     let e = document.getElementById("events");
                     e.innerHTML = json.page.totalElements + " events found.";
                     showEvents(json);
-                    initMap(position, json);
+                    
                  },
         error: function(xhr, status, err) {
                     console.log(err);
@@ -122,9 +128,59 @@ function infosubmit (alpha, omega, radiuss) {
 
 }
 
+function initMap2() {
+    let directionsRenderer = new google.maps.DirectionsRenderer;
+    let directionsService = new google.maps.DirectionsService;
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 7,
+      center: {lat: 41.85, lng: -87.65}
+    });
+    directionsRenderer.setMap(map);
+    directionsRenderer.setPanel(document.getElementById('right-panel'));
+
+    let control = document.getElementById('floating-panel');
+    control.style.display = 'block';
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
+
+    var onChangeHandler = function() {
+      calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+
+  }
+
+  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    let start = `${longlat[0].lat},${longlat[0].lng}`;
+    let end = `${addressGo}`;
+    directionsService.route({
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsRenderer.setDirections(response);
+      } else {
+        window.alert('Directions request failed due to ' + status);
+      }
+    });
+  }
+
+function goTo() {
+    $('#form2').click(event => {
+        event.preventDefault();
+        let place = $('#form2').val();
+        addressGo.push(place);
+        console.log(place);
+    });
+}
+
+
+
+
+
+
 function watchEnter() {
     datesinConsole();
-    $('form').submit(event => {
+    $('#form1').submit(event => {
         event.preventDefault();
         $('#errod').empty();
         $('.results').empty();
@@ -137,9 +193,11 @@ function watchEnter() {
         $('#map').removeClass('hidden');
         $('#events').removeClass('hidden');
         infosubmit(alpha, omega, radiuss);
+        
         console.log('hello');
         
     });
+    goTo();
 }
 
 $(watchEnter);
